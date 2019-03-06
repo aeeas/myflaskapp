64802dfaf9cbf866dc7ff8e0365a0aade6bef061
Alex Lag <alex@pop-os.localdomain>
2019-03-06 23:48:14 +0300

    Begin myflaskapp

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum enim minus quibusdam rem maiores. Fugit numquam sint omnis pariatur tempora molestiae sed maxime nemo! Dicta culpa omnis reiciendis vitae velit?',
+            'author': 'Alex Lag',
+            'create_date': '06-03-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum enim minus quibusdam rem maiores. Fugit numquam sint omnis pariatur tempora molestiae sed maxime nemo! Dicta culpa omnis reiciendis vitae velit?',
+            'author': 'John Doe',
+            'create_date': '06-03-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum enim minus quibusdam rem maiores. Fugit numquam sint omnis pariatur tempora molestiae sed maxime nemo! Dicta culpa omnis reiciendis vitae velit?',
+            'author': 'Alex Lag',
+            'create_date': '06-03-2019'
+        }
+    ]
+    return articles

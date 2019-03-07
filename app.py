8c7d05444f0663850d34e2661ffd21f2c5eacebf
Alex Lag <alex@pop-os.localdomain>
2019-03-07 23:22:53 +0300

    added authorization system

--- app.py
+++ app.py
@@ -3,28 +3,43 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 
 app = Flask(__name__)
 
+# config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'dev'
+app.config['MYSQL_PASSWORD'] = '123'
+app.config['MYSQL_DB'] = 'myflaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+# init MySQL
+mysql = MySQL(app)
+
 Articles = Articles()
 
+# index
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# about
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# articles
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles=Articles)
 
+# single article
 @app.route('/article/<string:id>')
 def article(id):
     return render_template('article.html', id=id)
 
+# register form class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -35,12 +50,85 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
+# user register
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        # create cursor
+        cur = mysql.connection.cursor()
+        # execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        # commit to DB
+        mysql.connection.commit()
+        # close connection
+        cur.close()
+        # flash message
+        flash('You are now registered and can log in', 'success')
+        # redirect to index page
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+# user login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+        # create cursor
+        cur = mysql.connection.cursor()
+        # get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        if result > 0:
+            # get store hash
+            data = cur.fetchone()
+            password = data['password']
+            # compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                session['logged_in'] = True
+                session['username'] = username
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)
+            # close connection
+            cur.close()
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error)
+    return render_template('login.html')
+
+# check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauhtorized, please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+
+
 if __name__ == '__main__':
+    app.secret_key='secret123'
     app.run(debug=True)
